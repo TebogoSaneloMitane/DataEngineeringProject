ba09b64cb1b24747abac74c9ca43c2d4dad00da6
beerger <william.berger.7099@student.uu.se>
2025-03-20 15:44:11 +0000

    renamed

---
+++ create_subsets.sh
@@ -0,0 +1,4 @@
+python3 DataEngineeringProject/subset_script.py "reddit_data/corpus-webis-tldr-17.json" "input/reddit_50k" 50000
+python3 DataEngineeringProject/subset_script.py "reddit_data/corpus-webis-tldr-17.json" "input/reddit_100k" 100000
+python3 DataEngineeringProject/subset_script.py "reddit_data/corpus-webis-tldr-17.json" "input/reddit_200k" 200000
+python3 DataEngineeringProject/subset_script.py "reddit_data/corpus-webis-tldr-17.json" "input/reddit_400k" 400000
